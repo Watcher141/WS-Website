6486498fb2134eb518bfca146e5498926f0fb749
Monojit <monojit0288@gmail.com>
2025-03-07 02:15:50 +0530

    adding debug log

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom"; // Change this line
+import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+console.log("✅ React app is initializing..."); // Debug log
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>  {/* Change BrowserRouter to HashRouter */}
+  <HashRouter>
     <App />
     <ToastContainer />
   </HashRouter>
 );
+
+console.log("✅ React app has been mounted!"); // Debug log
